f0f186cebb385bad27760e1ca0041877f10a751b
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-08-22 19:51:39 +0300

    feat: create all routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/about">{/* <About /> */}</Route>
-        <Route path="/users">{/* <Users /> */}</Route>
-        <Route path="/">{/* <Home /> */}</Route>
-      </Routes>
+      {/* <Routes>
+        <Route path="/" element={<Comp>main</Comp>} />
+        <Route path="/signin" element={<Comp>sign in</Comp>} />
+        <Route path="/signup" element={<Comp>signup</Comp>} />
+        <Route path="/textbook" element={<Comp>textbook</Comp>} />
+        <Route path="/statistic" element={<Comp>stat</Comp>} />
+        <Route path="/games/audiocall" element={<Comp>audiocall</Comp>} />
+        <Route path="/games/sprint" element={<Comp>sprint</Comp>} />
+        <Route path="*" element={<Comp>404</Comp>} />
+      </Routes> */}
     </div>
   );
 }
