816a116bc44e0827476eabbcd11b67ef395a6168
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-08-23 21:23:35 +0300

    feat: add props for WordCard

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -1,3 +1,27 @@
-export default function WordCard() {
-  return <div>WordCard</div>;
+type WordCardProps = {
+  word: string;
+  translate: string;
+  transcription: string;
+  textMeaning: string;
+  textExample: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
+  image: string;
+};
+
+export default function WordCard(props: WordCardProps) {
+  return (
+    <div>
+      <span>{props.word}, </span>
+      <span>{props.translate}, </span>
+      <span>{props.transcription}, </span>
+      <span>{props.textMeaning}, </span>
+      <span>{props.textExample}, </span>
+      <span>{props.textMeaningTranslate}, </span>
+      <span>{props.textExampleTranslate}</span>
+      <div>
+        Image: <span>{props.image}</span>
+      </div>
+    </div>
+  );
 }
--- src/pages/Textbook.tsx
+++ src/pages/Textbook.tsx
@@ -1,17 +1,53 @@
 /**
  * Textbook have 2 conditions:
  * auth and not auth => get this flag from userSlice
- * Components: 
+ * Components:
  * 1. Header
  * 2. Footer
- * 3. WordCard
- * 4. 
+ * 3. WordCard +
+ * 4.
  *
  */
 
+import WordCard from '../components/WordCard';
 import { useAppSelector } from '../hooks/storeHooks';
 
 export default function Textbook() {
   const isAuth = useAppSelector((store) => store.user.isAuth);
-  return <div>{isAuth ? <div>Textbook true</div> : <div>Textbook false</div>}</div>;
+  const mockWords = [
+    {
+      word: 'Word',
+      translate: 'translate',
+      transcription: 'transcription',
+      textMeaning: 'textMeaning',
+      textExample: 'textExample',
+      textMeaningTranslate: 'textMeaningTranslate',
+      textExampleTranslate: 'textExampleTranslate',
+      image: 'image',
+    },
+  ];
+  return (
+    <div>
+      {isAuth ? (
+        <div>Textbook true</div>
+      ) : (
+        <div>
+          {mockWords.map((item) => {
+            return (
+              <WordCard
+                word={item.word}
+                translate={item.translate}
+                transcription={item.transcription}
+                textMeaning={item.textMeaning}
+                textExample={item.textExample}
+                textMeaningTranslate={item.textMeaningTranslate}
+                textExampleTranslate={item.textExampleTranslate}
+                image={item.image}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
