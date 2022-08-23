6758ab519a7bbe50cfbfb81079ccc961bbb5d3e2
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-08-23 19:52:47 +0300

    feat: create Textbook.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Textbook from './pages/Textbook';
+
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/textbook" element={<Textbook />} />
+      </Routes>
       {/* <Routes>
         <Route path="/" element={<Comp>main</Comp>} />
         <Route path="/signin" element={<Comp>sign in</Comp>} />
         <Route path="/signup" element={<Comp>signup</Comp>} />
-        <Route path="/textbook" element={<Comp>textbook</Comp>} />
+        <Route path="/textbook" element={<Comp>textbook</Comp>} /> ++++++++++++++++++++
         <Route path="/statistic" element={<Comp>stat</Comp>} />
         <Route path="/games/audiocall" element={<Comp>audiocall</Comp>} />
         <Route path="/games/sprint" element={<Comp>sprint</Comp>} />
