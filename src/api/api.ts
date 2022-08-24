c014b963b57f676f6916d24a25c6bdbfd815914f
Ekateryna Sholomickaya <shan_lee@tut.by>
2022-08-24 22:04:59 +0300

    fix: fixup api

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,38 +5,66 @@ type User = {
   'password': string; 
 }
 
-
 export class API {
 
-  static host: string = 'https://react-learnwords-example.herokuapp.com';
+  static host: string = 'https://react-learnwords-rsl.herokuapp.com';
 
-  static async createUser(user: User) {
-    const userResponse =  await axios.post(this.host + '/users', user, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-    const pass = await userResponse.data;
-    return pass;
-  };
   
-  static async getUser(id: string) {
-    const userResponse = await axios.get(`${this.host}/users/${id}`);
-    return  userResponse;
-  }
-
-  static async getWords(page?:number, group?:number) {
+  /* ------ words api -------*/
+  static getWords(page?:number, group?:number) {
     if(page && group) 
       return axios.get(`${this.host}/words?page=${page}&group=${group}`);
     else return axios.get(`${this.host}/words`);
   }
   
-  static async getWord(id: string) {
-    const wordResponse = await axios.get(`${this.host}/words/${id}`);
-    return  wordResponse;
+  static getWord(id: string) {
+    return axios.get(`${this.host}/words/${id}`);
+  }
+  
+
+  /* ------ users api -------*/
+
+  static createUser(user: User) {
+
+    return  axios.post(this.host + '/users', user, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  
+  static getUser(id: string, token?: string) {
+
+    if(token) return axios.get(`${this.host}/users/${id}/token`);
+    return axios.get(`${this.host}/users/${id}`);
   }
 
+  /* ------ signin api -------*/
+
+  static signin(user: User){
+
+    return  axios.post(this.host + '/signin', user, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  /* ------ users/words api -------*/
+  
+  static getUsersWords(id: string, wordId: string) {
+    if (wordId) return axios.get(`${this.host}​/users/${id}​/words/${wordId}`);
+    return axios.get(`${this.host}​/users/${id}​/words`);
+  }
+  
+  /* ------ users/aggregatedWords api -------*/
+
+  static getAggregatedWords(id: string, wordId: string) {
+    if (wordId) return axios.get(`${this.host}​/users/${id}​/aggregatedWords/${wordId}`);
+    return axios.get(`${this.host}​/users/${id}​/aggregatedWords`);
+  }
 
 }
 
