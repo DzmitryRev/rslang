afa579c8bff9f416895ecdffd21c0d1b5c853ab7
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-08-26 18:36:42 +0300

    feat: add styles for PetalButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 // import { Routes, Route } from 'react-router-dom';
 
-import PrimaryButton from './components/primary-button/PrimaryButton';
+import PetalButton from './components/petal-button/PetalButton';
 
 function App() {
   return (
     <div className="App">
-      <PrimaryButton color="blue" size="s">
-        Войти
-      </PrimaryButton>
-
-      <PrimaryButton color="orange" size="m">
-        Учить
-      </PrimaryButton>
-
-      <PrimaryButton color="orange" size="m">
-        Зарегистрироваться
-      </PrimaryButton>
-
-      <PrimaryButton color="orange-gradient" size="l">
-        Сложное
-      </PrimaryButton>
-
-      <PrimaryButton color="blue-gradient" size="l">
-        Изучено
-      </PrimaryButton>
-      <PrimaryButton color="orange-gradient" size="xl">
-        Неверно
-      </PrimaryButton>
-
-      <PrimaryButton color="blue-gradient" size="xl">
-        Верно
-      </PrimaryButton>
+      <PetalButton shadowColor="blue" size="s" active={true}>A1</PetalButton>
+      <PetalButton shadowColor="light-purple" size="m">Привет</PetalButton>
+      <PetalButton shadowColor="purple" size="s">C2</PetalButton>
+      <PetalButton shadowColor="pink" size="m">Привет</PetalButton>
+      <PetalButton shadowColor="red" size="s">B1</PetalButton>
+      <PetalButton shadowColor="yellow" size="m">Привет</PetalButton>
       {/* <Routes>
         <Route path="/" element={<Comp>main</Comp>} />
         <Route path="/signin" element={<Comp>sign in</Comp>} />
--- src/components/petal-button/PetalButton.tsx
+++ src/components/petal-button/PetalButton.tsx
@@ -1,13 +1,44 @@
 import styles from './PetalButton.module.css';
 
 type PetalButtonProps = {
-  shadow: 'a';
-  size: 's' | 'l';
+  shadowColor: 'blue' | 'purple' | 'red' | 'light-purple' | 'yellow' | 'pink';
+  size: 's' | 'm';
+  active?: boolean;
+  callback?: () => void;
   children: React.ReactNode;
 };
 
-export default function PetalButton({ shadow, size, children }: PetalButtonProps) {
+export default function PetalButton({
+  shadowColor,
+  size,
+  active = false,
+  callback,
+  children,
+}: PetalButtonProps) {
   return (
-    <button className={`${styles.button} ${styles[shadow]} ${styles[size]}`}>{children}</button>
+    <button
+      className={`${styles.button} ${styles[shadowColor]} ${styles[size]} ${
+        active ? styles.active : ''
+      }`}
+      onClick={
+        callback
+          ? () => {
+            callback();
+          }
+          : () => {}
+      }
+    >
+      {children}
+    </button>
   );
+}
+
+// Пример:
+{
+  /* <PetalButton shadowColor="blue" size="s" active={true}>A1</PetalButton>
+<PetalButton shadowColor="light-purple" size="m">Привет</PetalButton>
+<PetalButton shadowColor="purple" size="s">C2</PetalButton>
+<PetalButton shadowColor="pink" size="m">Привет</PetalButton>
+<PetalButton shadowColor="red" size="s">B1</PetalButton>
+<PetalButton shadowColor="yellow" size="m">Привет</PetalButton> */
 }
