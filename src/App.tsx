f9a926945b4e9ac6fa5f1bf86de15a175f6f1682
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-08-26 17:14:16 +0300

    feat: change colors to vars in PrimaryButton.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 // import { Routes, Route } from 'react-router-dom';
 
+import PrimaryButton from './components/primary-button/PrimaryButton';
+
 function App() {
   return (
     <div className="App">
+      <PrimaryButton color="blue" size="s">
+        Войти
+      </PrimaryButton>
+
+      <PrimaryButton color="orange" size="m">
+        Учить
+      </PrimaryButton>
+
+      <PrimaryButton color="orange" size="m">
+        Зарегистрироваться
+      </PrimaryButton>
+
+      <PrimaryButton color="orange-gradient" size="l">
+        Сложное
+      </PrimaryButton>
+
+      <PrimaryButton color="blue-gradient" size="l">
+        Изучено
+      </PrimaryButton>
+      <PrimaryButton color="orange-gradient" size="xl">
+        Неверно
+      </PrimaryButton>
+
+      <PrimaryButton color="blue-gradient" size="xl">
+        Верно
+      </PrimaryButton>
       {/* <Routes>
         <Route path="/" element={<Comp>main</Comp>} />
         <Route path="/signin" element={<Comp>sign in</Comp>} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import './styles/vars.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
