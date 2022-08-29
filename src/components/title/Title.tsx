7ffe1f4437996085a6db94f969d4524647ae9540
Ekateryna Sholomickaya <shan_lee@tut.by>
2022-08-29 14:25:17 +0300

    feat:add titles

---
+++ src/components/title/Title.tsx
@@ -0,0 +1,17 @@
+﻿import styles from './Title.module.css';
+
+type TitleProps = {
+  align:'center'| 'start';
+  width:'info'|'dictionary'|'video'|'team'|'user';
+  children: React.ReactNode;
+}
+
+
+export default function Title({align,width, children}:TitleProps) {
+  return (
+    <h2 className={`${styles.titles} ${styles[align]} ${styles[width]}` }>
+      {children}
+    </h2>
+  );
+}
+
